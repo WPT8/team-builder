500172eea2b80d86d88afa6d399a81368f23d4b0
Douglas Little <douglas.a.little@gmail.com>
2019-09-10 21:24:49 -0400

    MVP, I think

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Member from './components/member';
+import Form from './components/form';
+
+
+function App() {
+  // const [user, setUser] = useState({ username: "", password: "" });
+
+  // const handleChange = event => {
+  //   setUser({ ...user, [event.target.name]: event.target.value });
+  // };
+
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+  //   console.log(user.name);
+  //   console.log(user.password);
+  // };
+
+  return (
+    <div className="App">
+      {/* {console.log(newMember)} */}
+      <Form />
+        {/* <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="text"
+            name="password"
+            value={user.password}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <button>Submit!</button>
+      </form> */}
+    </div>
+  );
+    };
+
+export default App;
